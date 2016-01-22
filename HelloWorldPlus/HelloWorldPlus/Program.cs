882851bad7c36227b1cbbe3045d1e07c7fb80ba3
Toad <172bandicoots@gmail.com>
2016-01-21 21:27:14 -0500

    My First GitHub Code - HelloWorldPlus

---
+++ HelloWorldPlus/HelloWorldPlus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Computer";
+            Console.WriteLine(a + " Says HELLO WORLD \n\n Please enter your name:");
+            Console.WriteLine("Hello " + Console.ReadLine());
+            Console.WriteLine("\n \n You can press any key to exit.");
+            Console.ReadLine(); //pause
+        }
+    }
+}
