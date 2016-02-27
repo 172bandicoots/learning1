2c6a603e4502845f24e6baa9b4a112108b22f263
Toad <172bandicoots@gmail.com>
2016-02-26 23:17:09 -0500

    SchoolDatabase Initial

---
+++ COP4834A02/COP4834SchoolDatabase/COP4834SchoolDatabase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COP4834SchoolDatabase.Startup))]
+namespace COP4834SchoolDatabase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
