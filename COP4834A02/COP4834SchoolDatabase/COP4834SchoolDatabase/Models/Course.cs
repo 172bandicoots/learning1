0348ffa1dc5f252d535fd2865c9eb5167332a3df
Toad <172bandicoots@gmail.com>
2016-03-12 01:12:55 -0500

    initial

---
+++ COP4834A02/COP4834SchoolDatabase/COP4834SchoolDatabase/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace COP4834SchoolDatabase.Models
+{
+    public class Course
+    {
+
+        [Key]
+        public int CourseID { get; set; }
+
+        [Required, Display(Name = "Title")]
+        public string CourseTitle { get; set; }
+
+        [Required, Display(Name = "Description")]
+        public String CourseDescription { get; set; }
+
+        [ForeignKey("Book")]
+        public int BookID { get; set; }
+
+    }
+}
---
+++ COP4834A02/COP4834SchoolDatabase/COP4834SchoolDatabase/Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace COP4834SchoolDatabase.Models
+{
+    public class Enrollment
+    {
+      //  [ForeignKey, Display(Name= "Course ID"),Required]
+     //   public CourseID { get; set; }
+
+    //    [ForeignKey("StudentID"),Display(Name = "Studen ID"), Required]
+     //   public int StudentID { get; set; }
+
+    }
+}
