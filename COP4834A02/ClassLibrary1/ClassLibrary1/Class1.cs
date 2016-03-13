6077db077f65c0a4546d799613de3d93f6754f64
Toad <172bandicoots@gmail.com>
2016-03-13 17:25:41 -0400

    Basic Database - clean and published.

---
+++ COP4834A02/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Description{ get; set; }
+        public bool IsEmployeedRetired { get; set; }
+        public string Country { get; set; }
+        public string Company { get; set; }
+    }
+}
